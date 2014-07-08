c6d44f1588869c4f659cb9a3131456900e6bb5d1
Francisco Facioni <fran6co@gmail.com>
2014-07-07 23:49:25 -0300

    Varios cambios:
     * Limpieza del encoding, estaba mal
     * Agrega la tabla de gente via Angular
     * Limpieza de los datos
     * Filtro por sexo
     * Filtro de edad se hace on the fly

--- static/js/map_alt.js
+++ static/js/map_alt.js
@@ -21,39 +21,93 @@ angular.module('app', ['localytics.directives'])
         .attr("width", width)
         .attr("height", height);
 
+    $scope.aportantes = [];
+
     queue()
         .defer(d3.json, "data/argentina.json")
-        .defer(d3.csv, "data/aportes_2013_all.csv")
+        .defer(d3.csv,  "data/aportes_2013.csv")
+        .defer(d3.csv,  "data/aportes_primarias_2013_geo.csv")
+        .defer(d3.csv,  "data/aportes_sexo_2013.csv")
         .await(ready);
 
-    function ready(error, argentina, aportes) {
+    function ready(error, argentina, aportes, aportes_geo, aportes_sexo) {
+        var geo = d3.nest()
+            .key(function(d){return d["CUIT/CUIL"];})
+            .map(aportes_geo, d3.map);
+
+        var sexo = d3.nest()
+            .key(function(d){return d["DOCUMENTO"]})
+            .map(aportes_sexo, d3.map);
+
+        aportes = aportes.map(function(aporte){
+            var key = aporte["CUIT/L"] || aporte["DOCUMENTO"];
+
+            var geo_data = geo.get(key);
+            if (geo_data && geo_data.length == 1) {
+                aporte = $.extend(aporte, geo_data[0]);
+            }
+
+            var sexo_data = sexo.get(aporte["DOCUMENTO"]);
+            if (sexo_data && sexo_data.length == 1) {
+                aporte = $.extend(aporte, sexo_data[0]);
+            }
+
+            return aporte;
+        });
+
         var mainData = aportes
             .filter(function(d){return d["AGRUPACION"] != '' && d["AGRUPACION"];})
-            .filter(function(d){return d["DEPARTAMENTO"] != '' && d["DEPARTAMENTO"];});
+            .filter(function(d){return d["DEPARTAMENTO"] != '' && d["DEPARTAMENTO"];})
+        ;
 
         var nest = d3.nest()
             .key(function(d) {return d["DEPARTAMENTO"];})
             .rollup(function(leaves) {return leaves.length;})
         ;
 
-        $scope.$apply(function(){
-            $scope.filters = {
-                agrupacion: {
-                    selected: null,
-                    values: sortKeysByValue(d3.nest()
-                        .key(function (d) {return d["AGRUPACION"];})
-                        .rollup(function(leaves) {return d3.sum(leaves, function(d){return parseFloat(d["IMPORTE"]);});})
-                        .map(mainData, d3.map), d3.descending)
-                },
-                edad: {
-                    selected: null,
-                    values: d3.nest()
-                        .key(function (d) {return d["EDAD"];})
-                        .rollup(function(leaves) {return d3.sum(leaves, function(d){return parseFloat(d["IMPORTE"]);});})
-                        .map(mainData, d3.map).keys().sort()
-                }
-            };
-        });
+        $scope.filters = {
+            agrupacion: {
+                selected: null,
+                values: sortKeysByValue(d3.nest()
+                    .key(function (d) {return d["AGRUPACION"];})
+                    .rollup(function(leaves) {return d3.sum(leaves, function(d){return parseFloat(d["IMPORTE"]);});})
+                    .map(mainData, d3.map), d3.descending)
+            },
+            edad: {
+                filter: function($field, selected) {return function (d) {
+                    var firstDigits = +d["DOCUMENTO"].split('').reverse().slice(6).reverse().join('');
+
+                    if (firstDigits == 19) {
+                        return false;
+                    } else if (firstDigits < 10) {
+                        return selected == $field.values[0];
+                    } else if (firstDigits < 17) {
+                        return selected == $field.values[1];
+                    } else if (firstDigits < 24) {
+                        return selected == $field.values[2];
+                    } else if (firstDigits < 31) {
+                        return selected == $field.values[3];
+                    } else {
+                        return selected == $field.values[4];
+                    }
+                };},
+                selected: null,
+                values: [
+                    "65 o más",
+                    "50 - 65",
+                    "40 - 49",
+                    "30 - 39",
+                    "29 o menos"
+                ]
+            },
+            sexo: {
+                selected: null,
+                values: [
+                    "F",
+                    "M"
+                ]
+            }
+        };
 
         var filters = {};
 
@@ -99,6 +153,8 @@ angular.module('app', ['localytics.directives'])
                 _data = _data.filter(filters[i]);
             }
 
+            $scope.aportantes = _data;
+
             var data = nest.map(_data, d3.map);
 
             scale.domain([d3.min(data.values())+1, d3.max(data.values())]);
@@ -127,20 +183,26 @@ angular.module('app', ['localytics.directives'])
             }
 
             render();
+            $scope.$apply();
         });
 
         for(var field in $scope.filters) {
-            $scope.$watch('filters.' + field + '.selected', function(field){return function(selected) {
+            $scope.$watch('filters.' + field + '.selected', function($field, field){return function(selected) {
                 if (selected) {
-                    filters[field] = function (d) {return d[field] == selected.toUpperCase();};
+                    if ($field.filter) {
+                        filters[field] = $field.filter($field, selected);
+                    } else {
+                        filters[field] = function (d) {return d[field] == selected.toUpperCase();};
+                    }
                 } else {
                     delete filters[field];
                 }
 
                 render();
-            }}(field.toUpperCase()));
+            }}($scope.filters[field], field.toUpperCase()));
         }
 
         render();
+        $scope.$apply();
     }
 });
