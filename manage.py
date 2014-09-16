ce28c592b51f9787e081fa1945da45bb60c7ecac
Gaba <gabelula@gmail.com>
2014-09-16 09:58:59 -0300

    Adds script to manage migrations

---
+++ manage.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+from app import models
+
+if __name__ == '__main__':
+    manager.run()
---
+++ migrations/versions/1991044f1f4f_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 1991044f1f4f
+Revises: None
+Create Date: 2014-09-16 09:50:12.385761
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1991044f1f4f'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
