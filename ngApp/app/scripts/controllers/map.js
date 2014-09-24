13bcb167b336187aec8f1a0839cd62663d40059c
Diego Nul <nuldiego@gmail.com>
2014-09-24 18:56:44 -0300

    refresh map on filter change

--- ngApp/app/scripts/controllers/map.js
+++ ngApp/app/scripts/controllers/map.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('larutadeldinero')
-    .controller('MapCtrl', function ($scope, $http, API) {
+    .controller('MapCtrl', function ($scope, $http, API,$rootScope) {
 
+    	$scope.layers=[];
         $scope.map = L.map('map', {
             center: [-34.5957733,-58.3814453],
             maxZoom: 17,
@@ -17,6 +18,8 @@ angular.module('larutadeldinero')
                 attribution: '&copy; ' + mapLink + ' Contributors'
             }).addTo($scope.map);
 
+        L.control.zoom().addTo($scope.map);
+
         $scope.center={
             lat: 24.0391667,
             lng: 121.525,
@@ -24,70 +27,87 @@ angular.module('larutadeldinero')
         };
 
 
-        var colors = ['FF0000', '0000FF','00FF00'];
+        var colors = ['868a08','386cb0','61380b','d7191c','018571','969696'];
         var markers=[];
-        markers[0]=[];
-        markers[1]=[];
-        markers[2]=[];
 
-        API.mapdata().then(function(response) {
-            var points = response.data.values;
-            console.log(points);        //TODO(gb): Remove trace!!!
-        });
-
-        $http.get('data/reduced.csv').success(function(data){
-            var lines = data.split('\n');
-            lines.splice(0,1);
-
-            var points = [];
-
-            for(var i=0;i<lines.length-2;i++){
-                var line = lines[i].split(',');
-                var layer=i%3?'northTaiwan':'southTaiwan';
-                points.push({'monto':line[1],'layer':layer,'lat':parseFloat(line[0]),'lng':parseFloat(line[2])});
-            }
-
-            for(var i = 0; i < points.length; i++){
-                var marker = new L.CircleMarker([points[i]['lat'],points[i]['lng']], {monto:points[i]['monto'],radius: 1+Math.log(points[i]['monto']), fillOpacity: 0.8, color: '#'+colors[i%3]}).bindPopup('layer '+i);
-                marker.l = i%3;
-                markers[i%3].push(marker);
-            }
-
-            for(var i = 0; i < markers.length; i++) {
-                var cluster = new L.MarkerClusterGroup({
-                    iconCreateFunction: function(cl) {
-                        var sum=0;
-                        cl.getAllChildMarkers().map(function(child){
-                            sum+=parseInt(child.options.monto);
-                        });
-                        var montoClass="1";
-                        if(sum<1000){
-                            montoClass=1;
-                        }else if( sum<20000){
-                            montoClass=2;
-                        }else if( sum<100000){
-                            montoClass=3;
-                        }else if( sum<1000000){
-                            montoClass=4;
-                        }else if( sum< 10000000){
-                            montoClass=5;
-                        }else{
-                            montoClass=6;
-                        }
-
-                        var layer = cl.getAllChildMarkers()[0].l;
-                        return new L.DivIcon({ html: '<div style="background-color:transparent; overflow:hidden"><div class=" layer'+layer+' circle' + montoClass + '"></div></div>' });
-                    },
-                    spiderfyDistanceMultiplier:	1,
-                    showCoverageOnHover:false,
-                    disableClusteringAtZoom:14,
-                    maxClusterRadius:50
-                });
-                for(var j = 0; j < markers[i].length; j++){
-                    cluster.addLayer(markers[i][j]);
-                }
-                console.log('adding cluster');
-                $scope.map.addLayer(cluster);
-            }
+        $rootScope.$on('filterChanged',function(event){
+        	$scope.addDataToMap();
         })
-    });
+        
+        
+
+		$scope.addDataToMap=function(){
+			if($scope.layers.length>0){
+				$scope.layers.forEach(function(d,i){
+					$scope.map.removeLayer(d);
+					console.log('refreshing map');
+				})
+				$scope.layers=[];
+			}
+
+			if($scope.loading){
+				return;
+			}
+			
+			$scope.loading=true;
+			API.mapdata().then(function(response) {
+	            var points = response.data.values;
+	            
+	            $(points).each(function(i,d){
+	            	if(!markers[d['color']]){
+	            		markers[d['color']]=[];
+	            	}
+	            	var item=d;
+	            	item['layer']=colors.indexOf(d['color']);
+	            	var marker = new L.CircleMarker([item['latitud'],item['longitud']], {monto:item['monto'],radius: Math.log(item['monto']), fillOpacity: 0.8, color: "#"+item['color']}).bindPopup('layer '+i);
+	                marker.l = colors.indexOf(d['color']);
+	                markers[d['color']].push(marker);
+	            })        //TODO(gb): Remove trace!!!
+
+	            for(var key in markers) {
+	                var cluster = new L.MarkerClusterGroup({
+	                    iconCreateFunction: function(cl) {
+	                        var sum=0;
+	                        cl.getAllChildMarkers().map(function(child){
+	                            sum+=parseInt(child.options.monto);
+	                        });
+	                        var montoClass="1";
+	                        if(sum<5000){
+	                            montoClass=1;
+	                        }else if( sum<20000){
+	                            montoClass=2;
+	                        }else if( sum<50000){
+	                            montoClass=3;
+	                        }else if( sum<100000){
+	                            montoClass=4;
+	                        }else if( sum< 500000){
+	                            montoClass=5;
+	                        }else{
+	                            montoClass=6;
+	                        }
+
+	                        var layer = cl.getAllChildMarkers()[0].l;
+	                        return new L.DivIcon({ html: '<div style="background-color:transparent; overflow:hidden"><div class=" layer'+layer+' circle' + montoClass + '"></div></div>' });
+	                    },
+	                    spiderfyDistanceMultiplier:	1,
+	                    showCoverageOnHover:false,
+	                    disableClusteringAtZoom:14,
+	                    maxClusterRadius:50
+	                });
+
+
+
+	                for(var j = 0; j < markers[key].length; j++){
+	                    cluster.addLayer(markers[key][j]);
+	                }
+	                $scope.layers.push(cluster);
+	                console.log($scope.layers);
+	                $scope.map.addLayer(cluster);
+	                $scope.loading=false;
+				}
+
+		});
+    
+    }
+});
+
