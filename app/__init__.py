ff7482ab992c8dc07cdc66440dc0178ad081db43
Gaba <gabelula@gmail.com>
2014-09-24 12:50:52 -0300

    Routes

--- app/__init__.py
+++ app/__init__.py
@@ -21,5 +21,12 @@ manager.create_api(Aportante, methods=['GET', 'POST'], allow_functions=True)
 manager.create_api(Aporte, methods=['GET'], allow_functions=True)
 manager.create_api(Agrupacion, methods=['GET'], include_columns=['id', 'nombre'], max_results_per_page=-1)
 
+# datos para visualizaciones
 app.add_url_rule('/api/treemap', 'treemap', data_for_treemap)
+app.add_url_rule('/api/map', 'map', data_for_map)
+
+# consultas
+app.add_url_rule('/api/aportantes/sexo', 'aportantes_por_sexo', aportantes_por_sexo)
+
+# info sobre el api
 app.add_url_rule('/', 'index', index)
