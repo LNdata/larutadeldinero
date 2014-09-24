68f9916f0101aabe2bdf9f83d52f26d155971bb0
Gaba <gabelula@gmail.com>
2014-09-24 10:30:17 -0300

    Starting new endpoints.

--- app/views.py
+++ app/views.py
@@ -17,55 +17,65 @@ from sqlalchemy import func
 # EndPoint treemap
 # EndPoint map data
 
-@app.route("/", methods = ['GET', 'POST'])
-@app.route("/viz/<viz>", methods = ['GET', 'POST'])
-def index(viz='treemap'):
-  params = request.args.to_dict()
-  page = request.args.get('page', 1, type=int)
+# @app.route("/", methods = ['GET', 'POST'])
+# @app.route("/viz/<viz>", methods = ['GET', 'POST'])
+# def index(viz='treemap'):
+#   params = request.args.to_dict()
+#   page = request.args.get('page', 1, type=int)
+#
+#   per_page = app.config.get('PER_PAGE', 10)
+#
+#   # TO DO - REFACTORING
+#   form = FilterForm(request.form, params)
+#   filters = get_filters(form.data)
+#
+#   aportes = get_donations(filters)
+#   aportes_paginados = aportes.paginate(page, per_page, False)
+#
+#   if (viz == 'mapa'):
+#     query = "select * from larutaelectoral where elecciones='GENERALES'"
+#     return render_template('mapa_b.html', aportes=aportes_paginados, form=form, query=query)
+#
+#   elif (viz == 'graficos'):
+#     return render_template('graficos_b.html', \
+#             aportes=aportes_paginados, form=form,\
+#             cantidad_aportantes_por_sexo=donors_per_sex(filters),\
+#             cantidad_aportantes_por_edad=donors_per_age(filters), \
+#             cantidad_aportantes_por_agrupaciones=donors_per_party(filters), \
+#             suma_aportes_por_sexo=import_per_sex(filters), \
+#             suma_aportes_por_edad=import_per_age(filters), \
+#             suma_aportes_por_agrupaciones=import_per_party(filters) \
+#             )
+#
+#   elif (viz == 'tabla'):
+#     return render_template('tabla_datos.html', aportes=aportes_paginados, form=form)
+#
+#   else:
+#     return render_template('treemap_b.html', aportes=aportes_paginados, form=form)
 
-  per_page = app.config.get('PER_PAGE', 10)
+@app.route('/api/aportantes/sexo')
+def aportantes_por_sexo():
 
-  # TO DO - REFACTORING
-  form = FilterForm(request.form, params)
-  filters = get_filters(form.data)
+  q = request.args.get('q')
+  filters = parse_filters(q)
 
-  aportes = get_donations(filters)
-  aportes_paginados = aportes.paginate(page, per_page, False)
+  cantidad_aportantes_por_sexo = donors_per_sex(filters),
 
-  if (viz == 'mapa'):
-    query = "select * from larutaelectoral where elecciones='GENERALES'"
-    return render_template('mapa_b.html', aportes=aportes_paginados, form=form, query=query)
+  aportes = {
+    "num_results": len(cantidad_aportantes_por_sexo),
+    'objects': cantidad_aportantes_por_sexo
+  }
 
-  elif (viz == 'graficos'):
-    return render_template('graficos_b.html', \
-            aportes=aportes_paginados, form=form,\
-            cantidad_aportantes_por_sexo=donors_per_sex(filters),\
-            cantidad_aportantes_por_edad=donors_per_age(filters), \
-            cantidad_aportantes_por_agrupaciones=donors_per_party(filters), \
-            suma_aportes_por_sexo=import_per_sex(filters), \
-            suma_aportes_por_edad=import_per_age(filters), \
-            suma_aportes_por_agrupaciones=import_per_party(filters) \
-            )
+  return jsonify( aportes )
 
-  elif (viz == 'tabla'):
-    return render_template('tabla_datos.html', aportes=aportes_paginados, form=form)
+@app.route('/api/treemap')
+def data_for_treemap():
 
-  else:
-    return render_template('treemap_b.html', aportes=aportes_paginados, form=form)
+  with open('data/treemap_elecciones.json','r') as f:
+    treemap_data = eval(f.read())
 
-@app.route('/aportante/<documento>')
-def aportante(documento):
-    aportante = Aportante.query.filter_by(documento=documento).first_or_404()
-    return render_template('aportante.html', aportante=aportante)
+  return jsonify(treemap_data)
 
-@app.route('/about')
-def about():
-  return render_template('sitio.html')
-
-@app.route('/faq')
-def faq():
-  return render_template('faq.html')
-
-@app.route('/team')
-def team():
-  return render_template('team.html')
+@app.route('/api/map')
+def data_for_map():
+  pass
