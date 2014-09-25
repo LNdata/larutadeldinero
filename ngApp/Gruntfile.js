6caf2e63c56114d48ed2ba8ffe82a38df753046b
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-09-25 20:54:08 -0300

    Github pages config

--- ngApp/Gruntfile.js
+++ ngApp/Gruntfile.js
@@ -338,7 +338,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'data/*'
           ]
         }, {
           expand: true,
