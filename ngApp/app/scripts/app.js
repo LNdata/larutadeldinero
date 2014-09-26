53397d4c0ad5ca7259351ffd2d3a36fc30fce9f4
andytow <andy@towsa.com>
2014-09-26 19:23:13 -0300

    Updated design
    table.html, aportante.html, added angular-filter, changed img dir

--- ngApp/app/scripts/app.js
+++ ngApp/app/scripts/app.js
@@ -16,7 +16,8 @@ angular
         'ngRoute',
         'ngSanitize',
         'ngTouch',
-        'ui.bootstrap'
+        'ui.bootstrap',
+		'angular.filter'
     ])
     .config(function ($routeProvider) {
         $routeProvider
